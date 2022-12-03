3c4ecadba00599d7c941ee49c54be013bb794e1a
keithkiama <keithkiama2003@gmail.com>
2022-12-04 01:57:47 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const init = () => {
     const inputForm = document.querySelector('form')
   
-    inputForm.addEventListener('submit', (submitEvent) => {
-      submitEvent.preventDefault();
+    inputForm.addEventListener('submit', (e) => {
+      e.preventDefault();
       const input = document.querySelector('input#searchByID');
   
       fetch(`http://localhost:3000/movies/${input.value}`)
